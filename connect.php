0e97a9712107301dfa3d1f0dd1ccb88eadf3aa0b
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-10-04 23:02:52 +0700

    add angular js frontend and php backend

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php 
+
+	$host = 'mysql:host=localhost;dbname=angularjscrud';
+	$username = 'root';
+	$password = '';
+
+	$db = new PDO($host, $username, $password);
+?>
---
+++ select.php
@@ -0,0 +1,12 @@
+<?php 
+
+	include "connect.php";
+
+	$sql = $db->prepare("select * from kota");
+	$sql->execute();
+
+	$murid = $sql->fetchall(PDO::FETCH_ASSOC);
+
+	print json_encode($murid);
+
+?>
